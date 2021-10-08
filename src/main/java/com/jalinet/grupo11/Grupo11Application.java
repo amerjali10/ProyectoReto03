5c268355ac41aa3e58e926a8be2bde61e5c3ebd1
= <=>
2021-10-08 14:30:37 -0500

    Server port on Servicio con /cat/all

--- src/main/java/com/jalinet/grupo11/Grupo11Application.java
+++ src/main/java/com/jalinet/grupo11/Grupo11Application.java
@@ -10,18 +10,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 //@ComponentScan(basePackageClasses = CustomeController.class)
-public class Grupo11Application {
+public class Grupo11Application1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Grupo11Application.class, args);
+		SpringApplication.run(Grupo11Application1.class, args);
 	}
       	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/all").allowedOrigins("http://132.226.240.254/");
-                                registry.addMapping("/save").allowedOrigins("http://132.226.240.254/");
+				registry.addMapping("/Custome/all").allowedOrigins("http://132.226.240.254/");
+                                registry.addMapping("/Custome/save").allowedOrigins("http://132.226.240.254/");
+				registry.addMapping("/Category/all").allowedOrigins("http://132.226.240.254/");
+                                registry.addMapping("/Category/save").allowedOrigins("http://132.226.240.254/");
 			}
 		};
 	}
